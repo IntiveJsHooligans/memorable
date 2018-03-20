c3df227588dc14770fdda45f34244cde78e08dc1
Mateusz Popławski <maty_1993@o2.pl>
2018-03-20 09:23:33 +0100

    button component refactor

--- src/app/shared/form/button/button.component.ts
+++ src/app/shared/form/button/button.component.ts
@@ -5,18 +5,8 @@ import { NgModule, Component, OnInit, Input } from '@angular/core';
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent  {
   @Input() content = "button"
   @Input() loading: boolean = false;
   @Input() styles: object;
-
-  defaultStyles = {
-    "font-size": '12px',
-    "border-radius": '30px'
-  }
-  
-  ngOnInit() {
-    this.styles = { ...this.defaultStyles, ...this.styles };
-  }
-
 }
